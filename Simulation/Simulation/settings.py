a185bcf6d0a9c9ece815f1386c6014fefac5d690
WeRiano <williamfors13@gmail.com>
2021-11-05 11:42:50 +0100

    woops

--- Simulation/Simulation/settings.py
+++ Simulation/Simulation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-73*exxdv15%j3wwd!2u-(fc60fi46(hpfp0**w%5f!g0=lvw4-'
+# SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
