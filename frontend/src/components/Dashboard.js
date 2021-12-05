d3c7a88654fcb0ea99caf1f33c20aa9cba29db48
WeRiano <williamfors13@gmail.com>
2021-12-05 22:56:03 +0100

    Started on dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,36 @@
 import React from "react";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <h1>This is the dashboard! Woo! </h1>
+      <Container>
+        <Row className="justify-content-center align-items-start">
+          <Col xs lg="3" sm="5">
+            <Card className="text-center"
+                  style={{marginTop: 100}}>
+              <Card.Header> Weather </Card.Header>
+              <Card.Body>
+                <Card.Text>Current weather speed: 5 m/s</Card.Text>
+                <Card.Text>Current temperature: -20 °C</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs lg="3" sm="5">
+            <Card className="text-center"
+                  style={{marginTop: 100}}>
+              <Card.Header> Economy </Card.Header>
+              <Card.Body>
+                <Card.Text>Current power production: 2 kWh</Card.Text>
+                <Card.Text>Current consumption: 2 kWh</Card.Text>
+                <Card.Text>Net production: 0 kWh</Card.Text>
+                <Card.Text>Buffer size: ???</Card.Text>
+                <Card.Text>Electricity Market Price: 0.65 kr/kWh</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
